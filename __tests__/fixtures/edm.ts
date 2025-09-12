3b1f3f70845b4c7b9e6687fddd9db941517a55be
Ladislav Zigo <ladislav.zigo@gmail.com>
2025-09-12 14:43:53 +0200

    test(fixtures): add EDM model and sample data

---
+++ __tests__/fixtures/edm.ts
@@ -0,0 +1,32 @@
+import type { EdmModel } from ../../src/core/types;
+
+export const model: EdmModel = {
+  namespace: Test,
+  entityTypes: [
+    {
+      name: Product,
+      key: [id],
+      properties: [
+        { name: id, type: Edm.Int32 },
+        { name: name, type: Edm.String },
+        { name: price, type: Edm.Decimal },
+        { name: categoryId, type: Edm.Int32 },
+      ],
+      navigation: [
+        { name: category, target: Category, collection: false },
+      ],
+    },
+    {
+      name: Category,
+      key: [id],
+      properties: [
+        { name: id, type: Edm.Int32 },
+        { name: title, type: Edm.String },
+      ],
+    },
+  ],
+  entitySets: [
+    { name: Products, entityType: Product },
+    { name: Categories, entityType: Category },
+  ],
+};
